1d2cecb805d21819c31e20ba35d7fb69de921e04
从不写作业 <35096105+FZer@users.noreply.github.com>
2019-08-08 17:40:35 +0800

    Add files via upload

--- flask_base/03_flask_HTTP/03_flask_HTTP.py
+++ flask_base/03_flask_HTTP/03_flask_HTTP.py
@@ -7,23 +7,26 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/index/')
+@app.route('/')
 def http_index():
-    return render_template('index.html')
+    return render_template('test01.html')
 
-@app.route('/success/<name01>/')
-def success(name01):
-    return "welcome %s" %name01
+@app.route('/success_post/<name01>/')
+def form_post(name01):
+    return "POST: %s" %name01
 
+@app.route('/success_get/<name02>/')
+def form_get(name02):
+    return "GET: %s" %name02
     
 @app.route('/index/',methods = ['POST','GET'])#表单的action的url必须和路由转换的地址一致；如'/index/'结尾都带有 '/',或者都不带。
 def index():
     if request.method == 'POST':
         user = request.form['username']
-        return redirect(url_for('success',name01 = user))
+        return redirect(url_for('form_post',name01 = user))
     else:
         user = request.args.get('username')
-        return redirect(url_for('success',name = user))
+        return redirect(url_for('form_get',name02 = user))
 
 if __name__ == '__main__':
     app.run(host="127.0.0.2",port=8088,debug=True)
